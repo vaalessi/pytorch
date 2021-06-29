acca71c7104702c676dc17ad9fef92c6705f3ee1
Zafar <cc.rafaz@zafar.cc>
2021-06-29 13:20:14 -0700

    [sparsity] WeightNormSparsifier
    Implements the weight norm sparsifier.
    This type of sparsifier computes the norm of the weights, sorts them, and zeroes-out the target fraction of them.
    
    Test Plan:
    
    ```
    python test/test_ao_sparsity.py
    ```
    
    ghstack-source-id: 3f18b3c07f175270c167a1445392247d499a302f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/58955

--- test/ao/sparsity/test_sparsifier.py
+++ test/ao/sparsity/test_sparsifier.py
@@ -2,8 +2,10 @@
 
 import logging
 
+import torch
 from torch import nn
-from torch.ao.sparsity import BaseSparsifier
+from torch.ao.sparsity import BaseSparsifier, WeightNormSparsifier
+from torch.nn.utils.parametrize import is_parametrized
 
 from torch.testing._internal.common_utils import TestCase
 
@@ -13,23 +15,28 @@ class Model(nn.Module):
     def __init__(self):
         super().__init__()
         self.seq = nn.Sequential(
-            nn.Linear(3, 4)
+            nn.Linear(16, 16)
         )
-        self.linear = nn.Linear(4, 3)
+        self.linear = nn.Linear(16, 16)
+
+    def forward(self, x):
+        x = self.seq(x)
+        x = self.linear(x)
+        return x
 
 
 class ImplementedSparsifier(BaseSparsifier):
     def __init__(self, **kwargs):
         super().__init__(defaults=kwargs)
 
-    def step(self):
-        pass
+    def update_mask(self, layer, **kwargs):
+        layer.mask[0] = 0
 
 
 class TestBaseSparsifier(TestCase):
     def test_constructor(self):
         # Cannot instantiate the base
-        self.assertRaisesRegex(TypeError, 'with abstract methods step',
+        self.assertRaisesRegex(TypeError, 'with abstract methods update_mask',
                                BaseSparsifier)
         # Can instantiate the model with no configs
         model = Model()
@@ -45,6 +52,14 @@ class TestBaseSparsifier(TestCase):
         assert 'test' in sparsifier.module_groups[0]
         assert sparsifier.module_groups[0]['test'] == 3
 
+    def test_step(self):
+        model = Model()
+        sparsifier = ImplementedSparsifier(test=3)
+        sparsifier.enable_mask_update = True
+        sparsifier.prepare(model, [model.linear])
+        sparsifier.step()
+        assert torch.all(model.linear.mask[0] == 0)
+
     def test_state_dict(self):
         model = Model()
         sparsifier0 = ImplementedSparsifier(test=3)
@@ -55,3 +70,37 @@ class TestBaseSparsifier(TestCase):
         assert sparsifier0.module_groups != sparsifier1.module_groups
         sparsifier1.load_state_dict(state_dict)
         assert sparsifier0.module_groups == sparsifier1.module_groups
+
+    def test_mask_squash(self):
+        model = Model()
+        sparsifier = ImplementedSparsifier(test=3)
+        sparsifier.prepare(model, [model.linear])
+        assert hasattr(model.linear, 'mask')
+        assert is_parametrized(model.linear, 'weight')
+        assert not hasattr(model.seq[0], 'mask')
+        assert not is_parametrized(model.seq[0], 'weight')
+
+        sparsifier.squash_mask()
+        assert not hasattr(model.seq[0], 'mask')
+        assert not is_parametrized(model.seq[0], 'weight')
+        assert not hasattr(model.linear, 'mask')
+        assert not is_parametrized(model.linear, 'weight')
+
+
+class TestWeightNormSparsifier(TestCase):
+    def test_constructor(self):
+        model = Model()
+        sparsifier = WeightNormSparsifier()
+        sparsifier.prepare(model, config=None)
+        for g in sparsifier.module_groups:
+            assert isinstance(g['module'], nn.Linear)
+            # The module_groups are unordered
+            assert g['path'] in ('seq.0', 'linear')
+
+    def test_logic(self):
+        model = Model()
+        sparsifier = WeightNormSparsifier(sparsity_level=0.5)
+        sparsifier.prepare(model, config=[model.linear])
+        sparsifier.enable_mask_update = True
+        sparsifier.step()
+        self.assertAlmostEqual(model.linear.mask.mean().item(), 0.5, places=2)
--- torch/ao/sparsity/__init__.py
+++ torch/ao/sparsity/__init__.py
@@ -1,15 +1,14 @@
-from . import sparsifier
-
-# Sparsifier
-from .sparsifier import BaseSparsifier
-
-# Parametrizations
-from .utils import FakeSparsity
-
 # Variables
 from ._mappings import get_sparse_mapping
 from ._mappings import get_dynamic_sparse_quantized_mapping
 from ._mappings import get_static_sparse_quantized_mapping
+
+# Sparsifier
+from .sparsifier.base_sparsifier import BaseSparsifier
+from .sparsifier.weight_norm_sparsifier import WeightNormSparsifier
+
+# Parametrizations
+from .sparsifier.utils import FakeSparsity
 
 # === Experimental ===
 
@@ -19,3 +18,4 @@ from .experimental.pruner.parametrization import ActivationReconstruction
 
 # Pruner
 from .experimental.pruner.base_pruner import BasePruner
+
